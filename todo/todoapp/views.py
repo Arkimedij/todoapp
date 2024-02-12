7734fb721ec0774ff2cafcd0a0e821463107c9da
Arkimedij <121115757+Arkimedij@users.noreply.github.com>
2024-02-12 14:05:23 +0545

    second commit

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse('<h1>Djanog</h1>')
-
+from .models import task
 def home(request):
-    return render(request,'home.html')
+    tasks = task.objects.all()
+    context = {'tasks': tasks}     
+    return render(request,'home.html',context)
+
+def tasklist(request):
+    return render(request,'task.html')
+
+def timer(request):
+    return render(request,'timer.html')

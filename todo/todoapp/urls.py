26ad91736ce50166cc6e91f52665363da1aaead2
Arkimedij <121115757+Arkimedij@users.noreply.github.com>
2024-02-14 22:30:40 +0545

    second commit

--- todo/todoapp/urls.py
+++ todo/todoapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('',views.tasklist, name = 'task'),
-    path('edit/<str:pk>/',views.edit, name = 'edittask'),
+    path('edit/<str:pk>/',views.edit, name = 'edit'),
     path('timer/',views.timer,name = 'timer'),
 ]
